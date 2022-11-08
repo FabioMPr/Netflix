85a212b7bd574a7c5b1830bafe6ee68346e02a11
alegitmad <alessandro.maddalena@students.uniroma2.eu>
2022-11-08 17:13:24 +0100

    boh

--- src/main/java/com/netflix/SchermataIniziale.java
+++ src/main/java/com/netflix/SchermataIniziale.java
@@ -4,4 +4,11 @@ public class SchermataIniziale {
     private String titoloContenuto;
     private boolean logged;
 
+    @Override
+    public String toString() {
+        return "SchermataIniziale{" +
+                "titoloContenuto='" + titoloContenuto + '\'' +
+                ", logged=" + logged +
+                '}';
+    }
 }
